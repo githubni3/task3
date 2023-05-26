808762bedf471f2e70973d2b724c613e0bdd0194
Nitin Kumar Saxena <saxenanitin72@gmail.com>
2023-05-26 20:02:28 +0530

    final

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
-const formHandler = (event, id) => {
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+const formHandler = async(event, id) => {
   event.preventDefault();
+  await delay(2000);
   document.getElementById(id).style.opacity = 0.4;
   document.getElementById(id).style.pointerEvents = "none";
+  document.getElementById(id).style.hover = 'none';
+  document.getElementById(id).style.focus = 'none';
 };
