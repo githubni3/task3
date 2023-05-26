46a3584634dc368892638f7590dc3ca525f0bb70
Nitin Kumar Saxena <saxenanitin72@gmail.com>
2023-05-26 19:22:17 +0530

    final

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
-const formHandler = (id)=>{
-    document.getElementById(id).disabled = true
-
-}
+const formHandler = (event, id) => {
+  event.preventDefault();
+  document.getElementById(id).style.opacity = 0.4;
+  document.getElementById(id).style.pointerEvents = "none";
+};
